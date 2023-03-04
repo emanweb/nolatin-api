d6149bafde979f420d600a1f8b70978e09f059f4
Emanuel Costa <emanweb@gmail.com>
2023-03-04 13:42:17 -0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,20 @@
+<form method="post" action="save.php">
+
+<label for="friendly_name">Friendly Name:</label>
+
+<input type="text" name="friendly_name" id="friendly_name">
+
+
+<label for="json_content">JSON Content:</label>
+
+<textarea name="json_content" id="json_content"></textarea>
+
+
+<label for="emailaddress">Email Address:</label>
+
+<input type="email" name="emailaddress" id="emailaddress">
+
+
+<input type="submit" value="Submit">
+
+</form>
