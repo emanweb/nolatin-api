e3f061e46c205bb325139e9eb84a6a70bccf2943
Emanuel Costa <emanweb@gmail.com>
2024-08-25 15:49:56 -0700

    replace function because json is encoded in the db

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include ('../../wo-config.php');
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 // Step 2: Write a SQL query to fetch the data from the MySQL table
 $friendly_name = mysqli_real_escape_string($conn, $_GET['friendly_name']);
-$sql = "SELECT json_content FROM nolatin_exports where friendly_name  = '".$friendly_name."'";
+$sql = "SELECT JSON_UNQUOTE(json_content) FROM nolatin_exports where friendly_name  = '".$friendly_name."'";
 // Step 3: Execute the SQL query using PHP
 $result = mysqli_query($conn, $sql);
 // Step 4: Convert the query result into a PHP array
@@ -13,7 +13,7 @@ while($row = mysqli_fetch_assoc($result)) {
     $rows[] = $row;
 }
 // Step 5: Convert the PHP array into JSON format
-$json_data = json_encode($rows);
+$json_data = json_decode($rows);
 // Step 6: Set the Content-Type header to application/json
 header('Content-Type: application/json');
 // Step 7: Output the JSON string
