bad5d964ebc3ab191303578be75c08b56b5bddb1
Marcello Paiva <marcello.paiva@gmail.com>
2024-09-03 16:00:16 -0400

    fixed update

--- update.php
+++ update.php
@@ -40,86 +40,60 @@ if ($_POST) {
   }
 
   // Escape special characters in the form data to prevent SQL injection attacks
-  $friendly_name = mysqli_real_escape_string($conn, $friendly_name);
-  $json_content = mysqli_real_escape_string($conn, $json_content);
-  $emailaddress = mysqli_real_escape_string($conn, $emailaddress);
+  // $friendly_name = mysqli_real_escape_string($conn, $friendly_name);
+  // $json_content = mysqli_real_escape_string($conn, $json_content);
+  // $emailaddress = mysqli_real_escape_string($conn, $emailaddress);
   
   // Check and post to backups table
     // Get backup list for friendly name
-    $backups_sql = "SELECT version FROM nolatin_backups WHERE friendly_name = '$friendly_name'";
+    $backups_sql = "SELECT version FROM nolatin_backups WHERE friendly_name = ?";
+    $backups_stmt = $conn->prepare($backups_sql);
+    $backups_stmt->bind_param("s", $friendly_name);
+    $backups_stmt->execute();
+    $backups_result = $backups_stmt->get_result();
 
-    $backups_result = mysqli_query($conn, $backups_sql);
-    $backups = array();
-    while($row = mysqli_fetch_assoc($backups_result)) {
-      $backups[] = $row;
-    }
-
-    if (count($backups) == 0) {
+    if ($backups_result->num_rows == 0) {
       // If none exist, create v0 & v1
         // Get current version
-        $current_sql = "SELECT json_content FROM nolatin_exports WHERE friendly_name = '$friendly_name'";
-        $current_result = mysqli_query($conn, $current_sql);
-        //TODO: FIX JSON CONVERSION
-        $current_json = mysqli_fetch_assoc($current_result)[0];
+        $current_sql = "SELECT json_content FROM nolatin_exports WHERE friendly_name = ?";
+        $current_stmt = $conn->prepare($current_sql);
+        $current_stmt->bind_param("s", $friendly_name);
+        $current_stmt->execute();
+        $current_result = $current_stmt->get_result();
+
+        $current_json = $current_result->fetch_all(MYSQLI_ASSOC);
+
         // Post v0
-        $post_v0_sql = "INSERT INTO nolatin_backups (friendly_name, json_content, version, status) VALUES ('$friendly_name', '$current_json', 0, 'inactive')";
-        try {
-          if (mysqli_query($conn, $post_v0_sql)) {
-            echo "v0 created successfully";
-          } else {
-            echo "Error: " . $post_v0_sql . "<br>" . mysqli_error($conn);
-          }
-        } catch (mysqli_sql_exception $e) {
-          echo "Error: " . $e;
-        }
+        $post_v0_sql = "INSERT INTO nolatin_backups (friendly_name, json_content, version, status) VALUES (?,?,?,?)";
+        $post_v0_stmt = $conn->prepare($post_v0_sql);
+        $post_v0_stmt->bind_param("ssis", $friendly_name, $current_json, 0, 'inactive');
+        $post_v0_stmt->execute();
+
         // Post v1
-        $post_v1_sql = "INSERT INTO nolatin_backups (friendly_name, json_content, version) VALUES ('$friendly_name', '$json_content', 1)";
-        try {
-          if (mysqli_query($conn, $post_v1_sql)) {
-            echo "v1 created successfully";
-          } else {
-            echo "Error: " . $post_v1_sql . "<br>" . mysqli_error($conn);
-          }
-        } catch (mysqli_sql_exception $e) {
-          echo "Error: " . $e;
-        }
+        $post_v1_sql = "INSERT INTO nolatin_backups (friendly_name, json_content, version) VALUES (?,?,?)";
+        $post_v1_stmt = $conn->prepare($post_v1_sql);
+        $post_v1_stmt->bind_param("ssi", $friendly_name, $json_content, 1);
+        $post_v1_stmt->execute();
     } else {
       // If backups do exist, create new version & set other versions to inactive
-        $set_old_inactive_sql = "UPDATE nolatin_backups SET status = 'inactive' WHERE friendly_name = '$friendly_name'";
-        try {
-          if (mysqli_query($conn, $set_old_inactive_sql)) {
-            echo "Set old versions to inactive successfully";
-          } else {
-            echo "Error: " . $set_old_inactive_sql . "<br>" . mysqli_error($conn);
-          }
-        } catch (mysqli_sql_exception $e) {
-          echo "Error: " . $e;
-        }
-        $version_num = count($backups);
-        $post_new_sql = "INSERT INTO nolatin_backups (friendly_name, json_content, version) VALUES ('$friendly_name', '$json_content', '$version_num')";
-        try {
-          if (mysqli_query($conn, $post_new_sql)) {
-            echo "v1 created successfully";
-          } else {
-            echo "Error: " . $post_new_sql . "<br>" . mysqli_error($conn);
-          }
-        } catch (mysqli_sql_exception $e) {
-          echo "Error: " . $e;
-        }
+      $update_old_sql = "UPDATE nolatin_backups SET status = 'inactive' WHERE friendly_name = ?";
+      $update_old_stmt = $conn->prepare($update_old_sql);
+      $update_old_stmt->bind_param("s", $friendly_name);
+      $update_old_stmt->execute();
+
+      $version_num = $backups_result->num_rows;
+
+      $post_new_sql = "INSERT INTO nolatin_backups (friendly_name, json_content, version) VALUES (?,?,?)";
+      $post_new_stmt = $conn->prepare($update_old_sql);
+      $post_new_stmt->bind_param("ssi", $friendly_name, $json_content, $version_num);
+      $post_new_stmt->execute();
     }
-  
     
   // Update original
-  $update_sql = "UPDATE nolatin_exports SET json_content = '$json_content' WHERE friendly_name = '$friendly_name'";
-  try {
-    if (mysqli_query($conn, $update_sql)) {
-      echo "Original updated successfully";
-    } else {
-      echo "Error: " . $update_sql . "<br>" . mysqli_error($conn);
-    }
-  } catch (mysqli_sql_exception $e) {
-    echo "Error: " . $e;
-  }
+  $update_original_sql = "UPDATE nolatin_exports SET json_content = ? WHERE friendly_name = ?";
+  $update_original_stmt = $conn->prepare($update_original_sql);
+  $update_original_stmt->bind_param("ss", $json_content, $friendly_name);
+  $update_original_stmt->execute();
 
   // Close the database connection
   mysqli_close($conn);
